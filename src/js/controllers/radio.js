b6718d5cb7dc8be7a8fd3ca38e3895466ce8671d
alexbowen <alexbowen123@googlemail.com>
2023-11-14 00:13:31 +0000

    mobile layout improvements

--- src/js/controllers/radio.js
+++ src/js/controllers/radio.js
@@ -4,7 +4,7 @@ const RadioController = class extends Controller {
   static targets = ['player'];
 
   connect() {
-    this.broadcastPlayer('https://embed.radio.co/player/b84cf70.html');
+    // this.broadcastPlayer('https://embed.radio.co/player/b84cf70.html');
   }
 
   play(e) {
