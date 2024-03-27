5539742eb4518f44a688560d83e81db085faf30b
alexbowen <alexbowen123@googlemail.com>
2024-03-27 12:08:50 +0000

    dsasad

--- src/js/controllers/radio.js
+++ src/js/controllers/radio.js
@@ -4,7 +4,16 @@ const RadioController = class extends Controller {
   static targets = ['player'];
 
   connect() {
-    this.broadcastPlayer('https://embed.radio.co/player/b84cf70.html');
+    const d = new Date();
+    const bdays = [5,6,7];
+    
+    let day = d.getDay();
+  
+    if ((d.getHours() >= 14 || d.getHours() <= 6) && bdays.includes(day)) {
+      this.broadcastPlayer('https://embed.radio.co/player/b84cf70.html');
+    }
+  
+    return false;
   }
 
   play(e) {
